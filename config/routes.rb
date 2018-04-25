f35a42946ec2439f1cf9a3342394bcd6bcd84e1a
Stuckey89 <antonio.stuckey1@gmail.com>
2018-04-24 20:10:12 -0500

    This is a Forum app using Ruby programming language on the Rails framework and Material Design.

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+	root to: 'discussions#index'
+  resources :discussions
+
+  get '/auth/:provider/callback' => 'sessions#new'
+  get '/auth/sign-out' => 'sessions#sign_out', as: :sign_out
+
+end
